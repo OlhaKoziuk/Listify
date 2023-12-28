54d828a13490ebae55a1d26dabebc421239c74be
OlhaKoziukDev <108747215+OlhaKoziuk@users.noreply.github.com>
2023-12-28 18:30:31 +0200

    clone listify application

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
-# React TypeScript Starter Pack
+# Listify
 
-To use this template click `Use this template`
+Welcome to Listify application! This is a simple and intuitive application where you can create, update, and delete todo lists, edit tasks, and mark them as completed.
 
-### Available Scripts
+## Technologies Used
 
-`Deploy` - available to deploy your application to gh-pages
+- HTML5
+- CSS3
+- Sass/SCSS
+- TypeScript
+- React
+- Classnames
 
-`SCSS Preprocessor` - available to write your styles with modern style language
+## Features
+
+- **Create Lists:** Easily create new to-do lists to organize your tasks.
+- **Update and Delete Lists:** Edit list names or remove them as needed.
+- **Add, Edit, and Delete Tasks:** Manage individual tasks within each list.
+- **Mark Tasks as Completed:** Keep track of your completed tasks.
+- **Responsive Design:** Access the app seamlessly across various devices.
+
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,104 @@
-import React from 'react';
-import './App.scss';
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React, { useState, useEffect, useMemo } from 'react';
+import { Header } from './components/Header';
+import { Todo } from './types/Todo';
+import { getTodos } from './api/todos';
+import { TodoList } from './components/TodoList';
+import { Footer } from './components/Footer/Footer';
+import { Error } from './components/Error';
+import { Filter } from './types/Filter';
 
-interface Props {
-  onClick: () => void;
-}
-
-export const Provider: React.FC<Props> = React.memo(
-  ({ onClick, children }) => (
-    <button
-      type="button"
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  ),
-);
+const USER_ID = 11830;
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [error, setError] = useState('');
+  const [filter, setFilter] = useState(Filter.All);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isHiddenClass, setIsHiddenClass] = useState(true);
+  const [query, setQuery] = useState('');
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+  const [completedTodos, setCompletedTodos] = useState<Todo[]>([]);
+  const [isDisable, setIsDisable] = useState(false);
+
+  useEffect(() => {
+    getTodos(USER_ID)
+      .then(setTodos)
+      .catch(() => {
+        setError('Unable to load todos');
+        setIsHiddenClass(false);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  const filteredTodos = useMemo(() => (
+    todos.filter(todo => {
+      switch (filter) {
+        case Filter.Active:
+          return !todo.completed;
+        case Filter.Completed:
+          return todo.completed;
+        default:
+          return true;
+      }
+    })
+  ), [todos, filter]);
+
+  useEffect(() => {
+    const completed = filteredTodos.filter(todo => todo.completed);
+
+    setCompletedTodos(completed);
+  }, [filteredTodos]);
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>
-        <TodoList />
-      </Provider>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+      <div className="todoapp__content">
+        <Header
+          query={query}
+          setQuery={setQuery}
+          setTodos={setTodos}
+          setError={setError}
+          setTempTodo={setTempTodo}
+          setIsHiddenClass={setIsHiddenClass}
+          todos={todos}
+          completedTodos={completedTodos}
+          isDisable={isDisable}
+          setIsDisable={setIsDisable}
+          isLoading={isLoading}
+        />
+
+        <TodoList
+          todos={filteredTodos}
+          setTodos={setTodos}
+          tempTodo={tempTodo}
+          setError={setError}
+          setIsHiddenClass={setIsHiddenClass}
+          setIsDisable={setIsDisable}
+        />
+
+        {todos.length !== 0 && (
+          <Footer
+            todos={todos}
+            setTodos={setTodos}
+            setFilter={setFilter}
+            completedTodos={completedTodos}
+            setError={setError}
+            setIsDisable={setIsDisable}
+            setIsHiddenClass={setIsHiddenClass}
+          />
+        )}
+      </div>
+
+      {!isLoading && (
+        <Error
+          error={error}
+          isHiddenClass={isHiddenClass}
+          setIsHiddenClass={setIsHiddenClass}
+        />
+      )}
     </div>
   );
 };
